5697751da05b5ccb1d1aa0ba5271f4875809c25b
Justin Kim <justinkim2015@gmail.com>
2023-01-23 20:21:57 +0900

    Split questions and text

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,18 +2,10 @@ class StoriesController < ApplicationController
   before_action :create_client
 
   def index
-    # resp = @client.completions(
-    #   parameters: {
-    #       model: "text-davinci-003",
-    #       # prompt: "Write a silly 1-paragraph story in simple english about a dog with reading comprehension questions",
-    #       prompt: 'say hi',
-    #       max_tokens: 3,
-    #       temperature: 0.8
-    #   })
-
+    @stories = Story.all
 
     if defined?(resp)
-      @text = response["choices"][0]["text"] 
+      @text = resp["choices"][0]["text"] 
     else
       @text = "No story for you right now! :("
     end 
@@ -21,10 +13,32 @@ class StoriesController < ApplicationController
 
   def new
     @story = Story.new
+
+    resp = @client.completions(
+      parameters: {
+          model: "text-davinci-003",
+          prompt: "Write a silly 1-paragraph story in simple english about #{params[:input]} with 3 reading comprehension questions",
+          # prompt: "type a synonym for #{params[:input]}",
+          max_tokens: 3000,
+          temperature: 0.8
+      })
+
+    @json = resp["choices"][0]
+
+    array = resp["choices"][0]["text"].split('Questions')
+    @text = array[0]
+    @quiz_array = array[1].split()
+
   end
 
   def create
     @story = Story.new(story_params) 
+
+    if @story.save
+      redirect_back_or_to @index
+    else 
+      render :new, status: :unprocessable_entity
+    end 
   end
 
   private 
