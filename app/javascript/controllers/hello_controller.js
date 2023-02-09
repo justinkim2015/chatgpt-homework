39ed9a9b4002e2a05d567450888c816d6adab336
Justin Kim <justinkim2015@gmail.com>
2023-02-09 14:28:34 +0900

    Used stimulus to toggle

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  static targets = [ "menu" ]
+
+  toggle() {
+    const menu = this.menuTarget
+    menu.classList.toggle('active')
+    menu.classList.toggle('inactive')
   }
 }
--- app/javascript/things/toggle.js
+++
@@ -1,9 +0,0 @@
-let hamburger = document.querySelector('.ham-button')
-let hamMenu = document.getElementById('ham-menu')
-
-function toggle() {
-  hamMenu.classList.toggle('active')
-  hamMenu.classList.toggle('inactive')
-}
-
-hamburger.addEventListener('click', toggle)
