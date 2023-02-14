d79443b9588193ab360e46ece27896a227b602d0
Justin Kim <justinkim2015@gmail.com>
2023-02-14 09:26:02 +0900

    add controller actions to stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,7 @@
 class StoriesController < ApplicationController
   before_action :create_client
-  before_action :get_story, only: [:show, :edit, :delete]
-  before_action :check_admin, only: [:delete]
+  before_action :get_story, only: [:show, :edit, :update, :delete]
+  # before_action :check_admin, only: [:delete]
 
   def index
     @stories = Story.all
@@ -9,6 +9,9 @@ class StoriesController < ApplicationController
     if defined?(resp)
     else
     end 
+  end
+
+  def show
   end
 
   def new
@@ -39,7 +42,24 @@ class StoriesController < ApplicationController
     end 
   end
 
-  def show
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to @story, notice: 'Story was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if current_user.admin? || current_user.id == @story.user_id 
+      @story.destroy
+      redirect_to root_path, notice: 'Story was successfully destroyed.'
+    else
+      redirect_to root_path, notice: "You don't have sufficient permissions to do this."
+    end  
   end
 
   private 
